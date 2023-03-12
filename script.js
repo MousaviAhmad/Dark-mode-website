8cd91d61059fc627d00e7acf670affc3d2a6c801
MousaviAhmad <116977632+MousaviAhmad@users.noreply.github.com>
2023-03-12 17:25:27 +0100

    Add dark mode and light mode, and make the website responsive - Used CSS to add dark mode and light mode to the website - Used JavaScript to toggle between the two modes based on user preference - Added media queries to make the website responsive for all devices

---
+++ script.js
@@ -0,0 +1,71 @@
+
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const navigatonBar = document.querySelector('#nav');
+const nav = document.getElementById("nav");
+const toggleIcon = document.getElementById("toggle-icon");
+const image1 = document.getElementById("image1");
+const image2 = document.getElementById("image2");
+const image3 = document.getElementById("image3");
+const textBox = document.getElementById('text-box');
+
+//Dark or Light Images
+function imageMode(color) {
+    image1.src = `img/innovative-${color}.svg`;
+    image2.src = `img/user-friendly-${color}.svg`;
+    image3.src = `img/innovative-${color}.svg`;
+}
+
+
+// Dark Mode Style
+function darkMode() {
+    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+    toggleIcon.children[0].textContent = 'Dark Mode';
+    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+    imageMode('dark')
+}
+
+
+// Light Mode Style
+function lightMode() {
+    nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+    textBox.style.backgroundColor = 'rgb(0 0 0 / 50% ) ';
+    toggleIcon.children[0].textContent = 'Light Mode';
+    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+    imageMode('light');
+}
+
+// Lite Mode Style
+
+
+// Switch Theme Dynamically
+function switchTheme(event) {
+    if (event.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+        darkMode();
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+        lightMode();
+    }
+}
+
+// Event Listener
+toggleSwitch.addEventListener('change', switchTheme);
+
+
+//Checke Local Storage For Theme
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        darkMode();
+    }
+}
+
+
+
+
+
